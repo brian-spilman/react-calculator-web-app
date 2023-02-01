b7e7d3a7071f5fba2adf639ea417c9d9aef6d7fd
brian.spilman <brian.spilman@wsu.edu>
2023-02-01 12:50:50 -0800

    fixed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { InputFunction } from './input-function';
 
 
 function App() {
   return <>
   
     <h1>Calculator App</h1>
+    <InputFunction></InputFunction>
   
   </>
 }
