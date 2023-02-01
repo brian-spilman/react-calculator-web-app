2eaceb173ab86e7bf9d8dcd1657e6c7ecf1ecf06
brian.spilman <brian.spilman@wsu.edu>
2023-02-01 12:43:56 -0800

    big functionality

---
+++ src/input-function.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+export function InputFunction(){
+
+    const [numbers, setNumbers] = useState({
+        numOne: 0,
+        numTwo: 0
+    });
+
+    const [results, setResults] = useState<number[]>([]);
+
+    function handleFirstNum(event: React.ChangeEvent<HTMLInputElement>){
+        const num:number = Number(event.target.value);
+        // We need to create a BRAND NEW OBJECT that has the desired value
+        
+        const numbersClone = {...numbers};
+        numbersClone.numOne = num;
+        setNumbers(numbersClone); // whenever you call a set function ALWAYS pass in a new object
+    }
+
+    function handleSecondNum(event: React.ChangeEvent<HTMLInputElement>){
+        const num:number = Number(event.target.value);
+        // We need to create a BRAND NEW OBJECT that has the desired value
+        
+        const numbersClone = {...numbers};
+        numbersClone.numTwo = num;
+        setNumbers(numbersClone); // whenever you call a set function ALWAYS pass in a new object
+    }
+
+    function doAddition(){
+        const resultsClone = [...results]; // clones the array
+        resultsClone.push(numbers.numOne + numbers.numTwo);
+        setResults(resultsClone);
+    }
+
+    return <>
+    
+        <label htmlFor="fNumInput">First Number</label>
+        <input id="textId" type="text" placeholder="0" onChange={handleFirstNum} size={30}></input>
+    
+        <label htmlFor="lNumInput">Second Number</label>
+        <input id="textId" type="text" placeholder="0" onChange={handleSecondNum} size={30}></input>
+
+        <button onClick={doAddition}>Add</button>
+        <button onClick={doSubtraction}>Subtract</button>
+        <button onClick={doMultiplication}>Multiply</button>
+        <button onClick={doDivision}>Division</button>
+
+        <ul>
+
+            {results.map(num => <li>{num}</li>)}
+
+        </ul>
+    </>
+
+
+
+}
